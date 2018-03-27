8eff5a5656d87447eda1eecc3e28f15876ee3f9e
Keshav <keshava.narasappa@rakuten.com>
2018-03-27 21:58:40 +0900

    update AR view

--- iOSProject/Views/AugmentedRealityViewController.swift
+++ iOSProject/Views/AugmentedRealityViewController.swift
@@ -13,9 +13,20 @@ import HDAugmentedReality
 
 final class AugmentedRealityViewController: UIViewController {
 
+    struct Constant {
+        static let results = "results"
+        static let geometryLocationLat = "geometry.location.lat"
+        static let geometryLocationLng = "geometry.location.lng"
+        static let reference = "reference"
+        static let name = "name"
+        static let vicinity = "vicinity"
+        static let formattedPhoneNumber = "formatted_phone_number"
+        static let result = "result"
+        static let website = "website"
+    }
+    
     @IBOutlet weak var mapView: MKMapView!
     
-    fileprivate let locationManager = CLLocationManager()
     fileprivate var startedLoadingPOIs = false
     fileprivate var places = [Place]()
     fileprivate var arViewController: ARViewController!
@@ -23,6 +34,7 @@ final class AugmentedRealityViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         let authorizationStatus = CLLocationManager.authorizationStatus()
@@ -42,15 +54,12 @@ final class AugmentedRealityViewController: UIViewController {
         arViewController.setAnnotations(places)
         self.present(arViewController, animated: true, completion: nil)
     }
-    
-    
 }
 
 extension AugmentedRealityViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if locations.count > 0 {
-            let location = locations.last!
-            print("Accuracy: \(location.horizontalAccuracy)")
+            guard let location = locations.last else { return }
             
             if location.horizontalAccuracy < 100 {
                 manager.stopUpdatingLocation()
@@ -61,22 +70,22 @@ extension AugmentedRealityViewController: CLLocationManagerDelegate {
                 if !startedLoadingPOIs {
                     startedLoadingPOIs = true
                     let loader = PlacesLoader()
-                    loader.loadPOIS(location: location, radius: 1000) { placesDict, error in
+                    loader.loadPOIS(location: location, radius: 1000) { [weak self] placesDict, error in
                         if let dict = placesDict {
-                            guard let placesArray = dict.object(forKey: "results") as? [NSDictionary]  else { return }
+                            guard let placesArray = dict.object(forKey: Constant.results) as? [NSDictionary]  else { return }
                             for placeDict in placesArray {
-                                let latitude = placeDict.value(forKeyPath: "geometry.location.lat") as! CLLocationDegrees
-                                let longitude = placeDict.value(forKeyPath: "geometry.location.lng") as! CLLocationDegrees
-                                let reference = placeDict.object(forKey: "reference") as! String
-                                let name = placeDict.object(forKey: "name") as! String
-                                let address = placeDict.object(forKey: "vicinity") as! String
+                                let latitude = placeDict.value(forKeyPath: Constant.geometryLocationLat) as! CLLocationDegrees
+                                let longitude = placeDict.value(forKeyPath: Constant.geometryLocationLng) as! CLLocationDegrees
+                                let reference = placeDict.object(forKey: Constant.reference) as! String
+                                let name = placeDict.object(forKey: Constant.name) as! String
+                                let address = placeDict.object(forKey: Constant.vicinity) as! String
                                 
                                 let location = CLLocation(latitude: latitude, longitude: longitude)
                                 let place = Place(location: location, reference: reference, name: name, address: address)
-                                self.places.append(place)
+                                self?.places.append(place)
                                 let annotation = PlaceAnnotation(location: place.location.coordinate, title: place.place)
                                 DispatchQueue.main.async {
-                                    self.mapView.addAnnotation(annotation)
+                                    self?.mapView.addAnnotation(annotation)
                                 }
                             }
                         }
@@ -93,7 +102,6 @@ extension AugmentedRealityViewController: ARDataSource {
         annotationView.annotation = viewForAnnotation
         annotationView.delegate = self
         annotationView.frame = CGRect(x: 0, y: 0, width: 150, height: 50)
-        
         return annotationView
     }
 }
@@ -102,13 +110,11 @@ extension AugmentedRealityViewController: AnnotationViewDelegate {
     func didTouch(annotationView: AnnotationView) {
         if let annotation = annotationView.annotation as? Place {
             let placesLoader = PlacesLoader()
-            placesLoader.loadDetailInformation(forPlace: annotation) { resultDict, error in
-                
-                if let infoDict = resultDict?.object(forKey: "result") as? NSDictionary {
-                    annotation.phoneNumber = infoDict.object(forKey: "formatted_phone_number") as? String
-                    annotation.website = infoDict.object(forKey: "website") as? String
-                    
-                    self.showInfoView(forPlace: annotation)
+            placesLoader.loadDetailInformation(forPlace: annotation) { [weak self] resultDict, error in
+                if let infoDict = resultDict?.object(forKey: Constant.result) as? NSDictionary {
+                    annotation.phoneNumber = infoDict.object(forKey: Constant.formattedPhoneNumber) as? String
+                    annotation.website = infoDict.object(forKey: Constant.website) as? String
+                    self?.showInfoView(forPlace: annotation)
                 }
             }
         }
